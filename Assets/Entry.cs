6d6cbfc30ac3a1099a29753094dace61b85d9c95
Mike Hayes <mghayes93@gmail.com>
2021-04-12 22:10:43 -0700

    new movement

--- Assets/Entry.cs
+++ Assets/Entry.cs
@@ -13,6 +13,25 @@ public struct PlayerState
 	public Vector2 velocity;
 	public Vector3 position;
 	public Vector3 cameraPosition;
+	public PlayerCollisionInfo  collisions;
+}
+
+public struct RaycastColliderOrigins
+{
+	public Vector2 bottomLeft, bottomRight;
+	public Vector2 topLeft, topRight;
+}
+
+public struct PlayerCollisionInfo
+{
+	public bool above, below;
+	public bool left, right;
+
+	public void Reset()
+	{
+		above = below = false;
+		left = right = false;
+	}
 }
 
 public class Entry : MonoBehaviour
@@ -24,7 +43,6 @@ public class Entry : MonoBehaviour
 	[SerializeField] Camera mainCamera;
 	[SerializeField] Transform playerTransform;
 	[SerializeField] BoxCollider2D playerCol;
-	[SerializeField] Rigidbody2D rb2d;
 	[SerializeField] SpriteRenderer playerSpriteRenderer;
 	[SerializeField] LayerMask interactableLayer;
 	[SerializeField] LayerMask groundLayer;
@@ -33,24 +51,15 @@ public class Entry : MonoBehaviour
 	[SerializeField, HideInInspector] InputActions inputActions;
 
 
-	// TODO: CLEAN UP THIS
-	public float minGroundNormalY = .65f;
-	public float gravityModifier = 1f;
-
-	private Vector2 targetVelocity;
-	private bool grounded;
-	private Vector2 groundNormal;
-	private Vector2 velocity;
-	private ContactFilter2D contactFilter;
-	private RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
-	private List<RaycastHit2D> hitBufferList = new List<RaycastHit2D> (16);
-
-	private const float minMoveDistance = 0.001f;
-	private const float shellRadius = 0.01f; // So we don't get stuck into another object
-	public float jumpTakeOffSpeed = 7;
+	// NEW MOVEMENT
 	const float skinWidth = 0.015f;
+	public RaycastColliderOrigins raycastColOrigins;
+	[SerializeField] int horizontalRayCount = 4;
+	[SerializeField] int verticalRayCount = 4;
+	float horizontalRaySpacing;
+	float verticalRaySpacing;
+	float velocityXSmoothing;
 
-	Vector3 move;
 	[SerializeField, HideInInspector] Vector2 lookDir;
 
 	void Awake() 
@@ -60,19 +69,19 @@ public class Entry : MonoBehaviour
 	}
 	void Start() 
 	{
-		contactFilter.useTriggers = false;
-		contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
-		contactFilter.useLayerMask = true;
-	}
+		Bounds bounds = playerCol.bounds;
+		bounds.Expand(skinWidth * -2);
 
-	// Update is called once per frame
-	void Update()
-	{
-		float t = Time.fixedTime;
-		float dt = Time.fixedDeltaTime;
-		targetVelocity = Vector2.zero;
-		ComputeVelocity ();
+		raycastColOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+		raycastColOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+		raycastColOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+		raycastColOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
 
+		horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+		verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+		horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+		verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
 	}
 
 	void FixedUpdate()
@@ -81,143 +90,94 @@ public class Entry : MonoBehaviour
 		float dt = Time.fixedDeltaTime;
 		InputSystem.Update();
 
-		// Movement
+		/*********** MOVEMENT ***********/
 		{
-			state.player.velocity += gravityModifier * Physics2D.gravity * dt;
-			state.player.velocity.x = targetVelocity.x;
-			grounded = false;
-			Vector2 deltaPosition = state.player.velocity * dt;
-			Vector2 moveAlongGround = new Vector2 (groundNormal.y, -groundNormal.x);
-			Vector2 move = moveAlongGround * deltaPosition.x;
+			if (state.player.collisions.above || state.player.collisions.below)
+			{
+				state.player.velocity.y = 0.0f;
+			}
 
-			Bounds bounds = playerCol.bounds;
-			bounds.Expand(skinWidth);
-			Vector2 bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
-			Vector2 bottomRight = new Vector2(bounds.max.x, bounds.min.y);
-			Vector2 topRight = new Vector2(bounds.max.x, bounds.max.y);
-			Vector2 topLeft = new Vector2(bounds.min.x, bounds.max.y);
+			UpdateRaycastOrigins();
+			// jumping
+			if (inputActions.Gameplay.Jump.triggered && state.player.collisions.below)
+			{
+				state.player.velocity.y = playerMoveSpec.jumpVelocity * dt;
+			}
 
-			if (inputActions.Gameplay.Move.triggered)
-				lookDir = inputActions.Gameplay.Move.ReadValue<Vector2>();
+			Vector2 xinput = inputActions.Gameplay.Move.ReadValue<Vector2>();
+			//state.player.velocity.x = xinput.x * playerMoveSpec.moveSpeed * dt;
+			float targetVelocityX = xinput.x * playerMoveSpec.moveSpeed * dt;
 			
+			state.player.velocity.x = Mathf.SmoothDamp(state.player.velocity.x, targetVelocityX, ref velocityXSmoothing, 
+					(state.player.collisions.below)?playerMoveSpec.accelerationTimeGrounded : playerMoveSpec.accelerationTimeAirborne);
+
+			state.player.velocity.y += playerMoveSpec.gravity * dt;
+
+
+			float dirY = Mathf.Sign(state.player.velocity.y);
 			float dirX = Mathf.Sign(state.player.velocity.x);
-			float rayLengthX = 1f;
+			float rayLengthY = Mathf.Abs(state.player.velocity.y) + skinWidth;
+			float rayLengthX = Mathf.Abs(state.player.velocity.x) + skinWidth;
 
-			Vector2 rayOrigin = (lookDir.x == -1) ? bottomLeft : bottomRight;
-			Vector2 rayInteractionOrigin = (lookDir.x == -1) ? topLeft : topRight;
 
-			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * lookDir.x, rayLengthX, groundLayer);
-			Debug.DrawRay(rayOrigin, Vector2.right * lookDir.x * rayLengthX, Color.blue);
-
-			RaycastHit2D interactableHit = Physics2D.Raycast(rayInteractionOrigin, Vector2.right * lookDir.x, playerMoveSpec.interactPhysicsRayLength);
-			Debug.DrawRay(rayInteractionOrigin, Vector2.right * lookDir.x * playerMoveSpec.interactPhysicsRayLength, Color.red);
-
-			if(hit)
+			state.player.collisions.Reset();
+			// Vertical Col
+			if (state.player.velocity.y != 0)
 			{
-				float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
-				if (slopeAngle > playerMoveSpec.maxSlopeAngle)
+				for (int i = 0; i < verticalRayCount; i++)
 				{
-					state.player.velocity.x = 0f;
-				}
-			}
+					Vector2 rayOrigin = (dirY == -1) ? raycastColOrigins.bottomLeft : raycastColOrigins.topLeft;
+					rayOrigin += Vector2.right * (verticalRaySpacing * i + state.player.velocity.x);
+					RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * dirY, rayLengthY, groundLayer);
 
+					Debug.DrawRay(rayOrigin , Vector2.up * dirY * rayLengthY, Color.red);
 
-			if (interactableHit)
-			{
-				// Box pushing
-				{
-					Box box = interactableHit.transform.GetComponent<Box>();
-					if (box)
+					if (hit)
 					{
-						if (box.isPushable)
-						{
-							//box.GetComponent<Rigidbody2D>().AddForce(state.player.velocity.normalized * 10f);
-							Rigidbody2D boxRb2d = box.GetComponent<Rigidbody2D>();
-							boxRb2d.position = (boxRb2d.position + move.normalized * (move.magnitude * playerMoveSpec.pushForce));
-							rb2d.position = (rb2d.position + move.normalized * (move.magnitude * playerMoveSpec.pushForce));
-							//boxRb2d.velocity += new Vector2(rb2d.velocity.x * 10f,0f);
-						}
+						state.player.velocity.y = (hit.distance - skinWidth) * dirY;
+						rayLengthY = hit.distance;
+						
+						state.player.collisions.below = dirY == -1;
+						state.player.collisions.above = dirY == 1;
 					}
 				}
 			}
-			Movement (move, false);
-			move = Vector2.up * deltaPosition.y;
-			Movement (move, true);
-			if (inputActions.Gameplay.Jump.triggered && grounded) {
-				//state.player.velocity.y = jumpTakeOffSpeed;
-				//rb2d.AddForce(Vector3.up * playerMoveSpec.jumpForce);
-				rb2d.velocity = new Vector2(rb2d.velocity.x, playerMoveSpec.jumpForce);
-			}
 
-		}
-		
-	}
-
-	void Movement(Vector2 move, bool yMovement)
-	{
-		float distance = move.magnitude;
-
-		if (distance > minMoveDistance) 
-		{
-			int count = rb2d.Cast (move, contactFilter, hitBuffer, distance + shellRadius);
-			hitBufferList.Clear ();
-			for (int i = 0; i < count; i++) {
-				hitBufferList.Add (hitBuffer [i]);
-			}
-
-			for (int i = 0; i < hitBufferList.Count; i++) 
+			// Horizontal Col
+			if (state.player.velocity.x != 0)
 			{
-				Vector2 currentNormal = hitBufferList[i].normal;
-
-				
-
-				if (currentNormal.y > minGroundNormalY) 
+				for (int i = 0; i < horizontalRayCount; i++)
 				{
-					grounded = true;
-					if (yMovement) 
+					Vector2 rayOrigin = (dirX == -1) ? raycastColOrigins.bottomLeft : raycastColOrigins.bottomRight;
+					rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+					RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * dirX, rayLengthX, groundLayer);
+
+					Debug.DrawRay(rayOrigin , Vector2.right * dirX * rayLengthX, Color.red);
+
+					if (hit)
 					{
-						groundNormal = currentNormal;
-						currentNormal.x = 0;
+						state.player.velocity.x = (hit.distance - skinWidth) * dirX;
+						rayLengthY = hit.distance;
+
+						state.player.collisions.left = dirX == -1;
+						state.player.collisions.right = dirX == 1;
 					}
 				}
-
-				float projection = Vector2.Dot (state.player.velocity, currentNormal);
-				if (projection < 0) 
-				{
-					state.player.velocity = state.player.velocity - projection * currentNormal;
-				}
-
-				float modifiedDistance = hitBufferList [i].distance - shellRadius;
-				distance = modifiedDistance < distance ? modifiedDistance : distance;
 			}
+
+			// Do translation after we handle out movement 
+			playerTransform.Translate(state.player.velocity);
 		}
-
-		rb2d.position = rb2d.position + move.normalized * distance;
 	}
 
-	private void OnGUI() {
-		GUI.Label(new Rect(0,0,100,100), move.ToString());
-		GUI.Label(new Rect(0,20,100,100), state.player.velocity.ToString());
-	}
-
-	void ComputeVelocity()
+	void UpdateRaycastOrigins()
 	{
-		if (grounded)
-			move = inputActions.Gameplay.Move.ReadValue<Vector2>();
-			
+		Bounds bounds = playerCol.bounds;
+		bounds.Expand(skinWidth * -2);
 
-		float vMax = playerMoveSpec.maximumMoveSped;
-
-		bool flipSprite = (playerSpriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
-		if (flipSprite) 
-		{
-			playerSpriteRenderer.flipX = !playerSpriteRenderer.flipX;
-		}
-
-		// animator.SetBool ("grounded", grounded);
-		// animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
-
-		//if (grounded)
-		targetVelocity = move * vMax;
+		raycastColOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+		raycastColOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+		raycastColOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+		raycastColOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
 	}
 }
--- Assets/Player/MoveSpec.cs
+++ Assets/Player/MoveSpec.cs
@@ -6,18 +6,20 @@ public class MoveSpec : ScriptableObject
 	[Header("Hover over fields with your mouse to see their description.")]
 
 	[Tooltip("The run speed of the character. Units: m/s")]
-	public float maximumMoveSped = 10f;
+	public float moveSpeed = .2f;
 
 	[Tooltip("The jump power of the character")]
-	public float jumpForce = 100f;
+	public float jumpVelocity = 8f;
 
 	[Tooltip("The max slope angle a character can move up")]
 	public float maxSlopeAngle = 70f;
 
-	[Tooltip("The force the chacter pushes at")]
-	public float pushForce = .2f;
+	[Tooltip("Gravity Scale")]
+	public float gravity = -1f;
 
-	[Tooltip("Physics interaction ray length")]
-	public float interactPhysicsRayLength = .2f;
+	[Tooltip("Change direction in air")]
+	public float accelerationTimeAirborne = -0.2f;
 
+	[Tooltip("Change direction on ground")]
+	public float accelerationTimeGrounded = -0.1f;
 }
